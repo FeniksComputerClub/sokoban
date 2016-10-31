2087d1e388735a80995176220667aef59324a341
Carlo <feniks.carlo@gmail.com>
2016-10-31 14:01:17 +0100

    Initial commit

---
+++ Board.cxx
@@ -0,0 +1,9 @@
+#include "sys.h"
+#include "BitBoard.h"
+
+using namespace cwchess;
+
+int main()
+{
+  BitBoard bb;
+}
---
+++ Board.h
@@ -0,0 +1,11 @@
+#pragma once
+
+class Board {
+  private:
+
+
+  public:
+    Board();
+
+
+};
